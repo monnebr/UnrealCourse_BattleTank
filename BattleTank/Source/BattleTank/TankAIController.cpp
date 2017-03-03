8133653b592b6e926e44a0ea421bad7c5b0104f3
Matteo Manelli <monnebr@gmail.com>
2017-03-03 13:46:02 +0100

    TankAIController find TankPLayerController

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -21,4 +21,29 @@ void ATankAIController::BeginPlay()
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("TankAIController do not control a tank!"));
 	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (PlayerTank) {
+		FString tankName = PlayerTank->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player tank %s"), *tankName);
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController cannot find player tank!"));
+	}
+
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	APlayerController* playerController = GetWorld()->GetFirstPlayerController();
+	if (!playerController) {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController could not find player controller!"));
+		return nullptr;
+	}
+	APawn* playerTank = playerController->GetPawn();
+	if (!playerTank) {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController could not find player tank!"));
+		return nullptr;
+	}
+	return Cast<ATank>(playerTank);
 }
